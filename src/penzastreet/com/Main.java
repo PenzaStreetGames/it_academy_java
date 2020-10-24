d1a83303ec2e3c89cc0999d2d71925fad355a7a7
Pavel Solomatin <crave_ozer_man@mail.ru>
2020-10-24 19:13:43 +0300

    Auto stash before merge of "main" and "origin/main"

--- src/penzastreet/com/Main.java
+++ src/penzastreet/com/Main.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Nikolaevich!");
-        add();
+        //add();
+        //abc();
+        weights();
     }
 
     public static void add() {
@@ -14,6 +16,24 @@ public class Main {
         b = in.nextInt();
         int c = a + b;
         System.out.println(c);
+    }
+
+    public static void abc() {
+        int a = 5, b, c;
+        b = a++;
+        c = ++a;
+        System.out.println(a + "\n" + b + "\n" + c);
+    }
+
+    public static void weights() {
+        int N, res = 0;
+        Scanner in = new Scanner(System.in);
+        N = in.nextInt();
+        while (N > 0) {
+            N >>= 1;
+            res++;
+        }
+        System.out.println(res - 1);
     }
 }
 
